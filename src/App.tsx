2aae24e8ed5ddaa4eba987443667c6958ce8b3bd
George Bockari <bit@revisioned.consulting>
2025-05-09 16:09:00 -0400

    refactor: add cruip header, list container and content-placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ContentPlaceholder } from "@/components/content-placeholder";
+import { Navbar } from "@/components/navbar";
+
+const items = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  // More items...
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="py-24 sm:py-32 lg:py-40">
+      <Navbar />
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+        <div className="mx-auto max-w-2xl">
+          {/* Content goes here */}
+          <ul role="list" className="space-y-3">
+            {items.map((item) => (
+              <li
+                key={item.id}
+                className="overflow-hidden rounded-md bg-white px-6 py-4 shadow-sm"
+              >
+                {/* Your content */}
+                <ContentPlaceholder className="h-24" />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
